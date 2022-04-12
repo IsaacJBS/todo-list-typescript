eb5cc242654ee1435c244c5867fd19a4f4594def
Isaac <isaac.jordaobs@gmail.com>
2022-04-12 16:41:52 -0300

    feat: formulario de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import styles from './App.module.css'
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <main className={styles.main}>
+        
+        <div className="">
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText='Criar tarefa' />
+        </div>
+        <div className="">
+          <h2>Suas tarefas</h2>
+          <TaskList />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
